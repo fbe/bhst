4c2904e5479ae75860e92bfea3381e935383350e
Felix Becker <git@felixbecker.name>
2012-05-20 01:29:18 +0200

    nohup bash

--- src/test/java/name/felixbecker/buildhivesecuritytest/NoHupAnyoneTest.java
+++ src/test/java/name/felixbecker/buildhivesecuritytest/NoHupAnyoneTest.java
@@ -7,7 +7,7 @@ public class NoHupAnyoneTest extends CriminalParent {
 	@Test // start top in test, expect no termination after jenkins finished! lets try..
 	public void nohupWhereAreYouMyFriend() throws Exception{
 		execute("file", "/usr/bin/nohup");
-		execute("nohup","top");
+		execute("nohup","bash");
 		execute("ps", "ax");
 	}
 }
