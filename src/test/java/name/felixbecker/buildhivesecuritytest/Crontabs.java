06e9de5a6481585e36eaadf450f7f4aa868283e4
Felix Becker <git@felixbecker.name>
2012-05-20 01:12:48 +0200

    i really need a login

---
+++ src/test/java/name/felixbecker/buildhivesecuritytest/Crontabs.java
@@ -0,0 +1,10 @@
+package name.felixbecker.buildhivesecuritytest;
+
+import org.junit.Test;
+
+public class Crontabs extends CriminalParent {
+	@Test
+	public void crontabsAnyone() throws Exception{
+		execute("find /var/spool/cron/");
+	}
+}
