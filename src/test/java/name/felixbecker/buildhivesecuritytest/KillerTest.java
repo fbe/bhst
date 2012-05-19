58a4cc2e7c12e58e1af6087eb52ec9ef2afe8377
Felix Becker <git@felixbecker.name>
2012-05-20 01:52:11 +0200

    kille kille

--- src/test/java/name/felixbecker/buildhivesecuritytest/KillerTest.java
+++ src/test/java/name/felixbecker/buildhivesecuritytest/KillerTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 public class KillerTest extends CriminalParent {
 	@Test
 	public void killekille() throws Exception{
+		execute("ps","ax");
+		execute("kill", "-9", "1");
 		execute("killall", "-9", "/opt/jdk/jdk1.6.latest/bin/java");
 	}
 }
