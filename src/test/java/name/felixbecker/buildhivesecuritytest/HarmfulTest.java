9541cc303f4c192ff896e8bbfc98734597c4ca47
Felix Becker <git@felixbecker.name>
2012-05-19 04:45:14 +0200

    gimme all your files

--- src/test/java/name/felixbecker/buildhivesecuritytest/HarmfulTest.java
+++ src/test/java/name/felixbecker/buildhivesecuritytest/HarmfulTest.java
@@ -15,7 +15,24 @@ public class HarmfulTest {
 		printFileContents("/proc/mounts");
 	}
 	
+	@Test
+	public void execCommands() throws Exception {
+		execute("find", "/");
+	}
 	
+	
+	private void execute(String... command) throws Exception {
+		System.out.println("Executing " + command);
+		ProcessBuilder processBuilder = new ProcessBuilder(command);
+		Process p = processBuilder.start();
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String line;
+		while((line = bufferedReader.readLine()) != null){
+			System.out.println(line);
+		}
+		bufferedReader.close();
+	}
+
 	private void printFileContents(String filePath) throws Exception {
 		File file = new File(filePath);
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
@@ -23,6 +40,7 @@ public class HarmfulTest {
 		while((x = bufferedReader.readLine()) != null){
 			System.out.println(x);
 		}
+		bufferedReader.close();
 	}
 	
 }
